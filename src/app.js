e902623bd96f8b4c4060589ef85e06e897c6f2a7
Lautaro <107657857+tatidee@users.noreply.github.com>
2024-07-04 13:30:44 +0000

    Coding Guidelines Challenge

--- src/app.js
+++ src/app.js
@@ -1,24 +1,19 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
+//arrays de excusas
 document.addEventListener("DOMContentLoaded", function() {
   let who = ["The dog", "My grandma", "The mailman", "My bird"];
   let action = ["ate", "peed", "crushed", "broke"];
   let what = ["my homework", "my phone", "the car"];
-  let when = [
-    "before the class",
-    "when I was sleeping",
-    "while I was exercising",
-    "during my lunch",
-    "while I was praying"
-  ];
+  let when = ["before the class","when I was sleeping","while I was exercising","during my lunch","while I was praying",];
+  //Obtener un elemento al azar de un array
   function getRandomElement(array) {
     return array[Math.floor(Math.random() * array.length)];
   }
+  //Variable para obtener la excusa completa
   let excuse = `${getRandomElement(who)} ${getRandomElement(
     action
   )} ${getRandomElement(what)} ${getRandomElement(when)}.`;
